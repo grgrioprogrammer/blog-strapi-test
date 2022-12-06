59ae66ca43f907c7ec9ef43ce8f1f380012b3634
gregorius <benjadeveloper@gmail.com>
2022-12-06 01:45:54 -0300

    another

--- blog-page/components/Header.js
+++ blog-page/components/Header.js
@@ -6,6 +6,12 @@ const Header = () => {
         <header>
             <div>
                 <Image src="/logo.svg" width={170} height={60} alt="logo"/>
+                <nav>
+                    <Link href="/">Home</Link>
+                    <Link href="/about">About</Link>
+                    <Link href="/blog">Blog</Link>
+                    <Link href="/contact">Contact</Link>
+                </nav>
             </div>
         </header>
     )
