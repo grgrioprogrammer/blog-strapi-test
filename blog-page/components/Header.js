21ecd57825e04e1599b9d1e69157f316f76ee6cf
Gregorio Benjamin Gullo <benjadeveloper@gmail.com>
2022-12-06 14:34:46 -0300

    commit from notebook

--- blog-page/components/Header.js
+++ blog-page/components/Header.js
@@ -1,11 +1,14 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import styles from '../styles/Home.modules.css'
 
 const Header = () => {
     return (
-        <header>
-            <div>
-                <Image src="/logo.svg" width={170} height={60} alt="logo"/>
+        <header className={styles.header}>
+            <div className={`container ${styles.menuLogo}`}>
+                <Link>
+                     <Image src="/logo.svg" width={170} height={60} alt="logo"/>
+                </Link>
                 <nav>
                     <Link href="/">Home</Link>
                     <Link href="/about">About</Link>
